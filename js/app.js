9e64fda689f2bf9b4c4d8da16d823dd7a17716bf
Pandey <kispande@38f9d3647b46.ant.amazon.com>
2019-04-24 11:49:59 -0700

    worked on images

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 $( document ).ready(function() {
-  $.getJSON("../lab-02-repository/data/page-1.json", function(data) {
+  $.getJSON('./data/page-1.json', function(data) {
     Object.keys(data).forEach(function(k){
       console.log(data[k]);
-      $("#photo-template").append('<img src="' + data[k].image_url + '" />');
-
+      $('#photo-template').append('<img src="' + data[k].image_url + '" />');
+      $('img').addClass( 'landscape');
     });
   });
 });
